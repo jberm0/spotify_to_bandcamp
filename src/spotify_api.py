f64832f9489f7fa517f06e6d93b362ac28066c7a
jberm0 <jmberman12@gmail.com>
2025-11-15 15:15:40 +0000

    auth

--- src/spotify_api.py
+++ src/spotify_api.py
@@ -19,11 +19,33 @@ scope = "playlist-read-private playlist-read-collaborative user-library-read use
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=SPOTIPY_CLIENT_ID,
-        client_secret=SPOTIPY_CLIENT_SECRET,
-        redirect_uri=SPOTIPY_REDIRECT_URI,
-        scope="playlist-read-private playlist-read-collaborative user-library-read user-top-read user-read-recently-played",
-    )
+# sp = Spotify(
+#     auth_manager=SpotifyOAuth(
+#         client_id=SPOTIPY_CLIENT_ID,
+#         client_secret=SPOTIPY_CLIENT_SECRET,
+#         redirect_uri=SPOTIPY_REDIRECT_URI,
+#         scope="playlist-read-private playlist-read-collaborative user-library-read user-top-read user-read-recently-played",
+#     )
+# )
+
+sp_oauth = SpotifyOAuth(
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET,
+    redirect_uri=SPOTIPY_REDIRECT_URI,
+    scope=scope,
 )
+
+
+query_params = st.query_params()
+if "code" in query_params:
+    # If we have the code, we can get the access token
+    auth_code = query_params["code"][0]
+    token_info = sp_oauth.get_access_token(auth_code)
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+
+    st.success("Successfully authenticated with Spotify!")
+    # You can now use `sp` to interact with Spotify API
+else:
+    # If no code is present, the user hasn't authorized the app yet
+    auth_url = sp_oauth.get_authorize_url()
+    st.markdown(f"[Click here to authenticate with Spotify]({auth_url})")
