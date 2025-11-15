9f87e4035fa9a5606a5f1a2ffd52ce94e180806f
jberm0 <jmberman12@gmail.com>
2025-11-15 15:11:11 +0000

    st secrets

--- src/spotify_api.py
+++ src/spotify_api.py
@@ -7,11 +7,23 @@ SPOTIPY_REDIRECT_URI = st.secrets["SPOTIPY_REDIRECT_URI"]
 
 scope = "playlist-read-private playlist-read-collaborative user-library-read user-top-read user-read-recently-played"
 
-sp = spotipy.Spotify(
-    auth_manager=spotipy.SpotifyOAuth(
+# sp = spotipy.Spotify(
+#     auth_manager=spotipy.SpotifyOAuth(
+#         client_id=SPOTIPY_CLIENT_ID,
+#         client_secret=SPOTIPY_CLIENT_SECRET,
+#         redirect_uri=SPOTIPY_REDIRECT_URI,
+#         scope=scope,
+#     )
+# )
+
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = Spotify(
+    auth_manager=SpotifyOAuth(
         client_id=SPOTIPY_CLIENT_ID,
         client_secret=SPOTIPY_CLIENT_SECRET,
         redirect_uri=SPOTIPY_REDIRECT_URI,
-        scope=scope,
+        scope="playlist-read-private playlist-read-collaborative user-library-read user-top-read user-read-recently-played",
     )
 )
